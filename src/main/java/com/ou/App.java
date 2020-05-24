064f8e4230ba73ef1ba055bbddadffd5db5d37b8
king <55868342@qq.com>
2020-05-24 20:58:16 +0800

    branch:king.v0.5

--- src/main/java/com/ou/App.java
+++ src/main/java/com/ou/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! v0.1" );
+        System.out.println( "Hello World! v0.1 branch:king.v0.5" );
     }
 }
